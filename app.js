5857a2cc6b5ae5c5a01b0f90b3f3a52b5fcb06ad
Patel Vishal <119497018+Vishu0617@users.noreply.github.com>
2023-04-15 14:17:39 +0530

    commit file

--- app.js
+++ app.js
@@ -18,12 +18,12 @@ app.use(express.static("Upload/User"));
 //databse connection
 
 //static file read
-app.use(express.static(path.join(__dirname, '../front/build')));
+// app.use(express.static(path.join(__dirname, '../front/build')));
 
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../front/build/index.html'));
-});
-app.listen(9000);
+// app.get('*', function (req, res) {
+//   res.sendFile(path.join(__dirname, '../front/build/index.html'));
+// });
+// app.listen(9000);
 
 
 const DB = process.env.DATABASE;
